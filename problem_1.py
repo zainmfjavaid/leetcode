1e4f263fa922b1577f6cf1f9fe76276d46552352
Zain Javaid <zainmfj@gmail.com>
2023-12-08 21:24:35 -0600

    Added solution for problem 1

---
+++ problem_1.py
@@ -0,0 +1,15 @@
+# Solution Runtime: 61ms
+
+from typing import List
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    r_dict = {}
+    for i, num in enumerate(nums):
+        remainder = target - num
+        if remainder in r_dict:
+            return [i, r_dict[remainder]]
+        else:
+            r_dict[num] = i
+            
+if __name__ == '__main__':
+    print(two_sum([3,2,4], 6))
