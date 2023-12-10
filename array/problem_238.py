f7a0015226405c4647ba50bc1452254f8e46c3e4
Zain Javaid <zainmfj@gmail.com>
2023-12-09 23:23:36 -0600

    Moved solutions to array directory

---
+++ array/problem_238.py
@@ -0,0 +1,16 @@
+# Problem 238: Product of Array Except Self
+# Solution Runtime: 
+
+from typing import List
+
+def product_except_self(nums: List[int]) -> List[int]:
+    products = [1] * len(nums)
+    for i in range(1, len(nums)):
+        products[i] = products[i - 1] * nums[i - 1]
+
+    suffix = nums[-1]
+    for i in range(len(nums) - 2, -1, -1):
+        products[i] *= suffix
+        suffix *= nums[i]
+    
+    return products
